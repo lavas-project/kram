4e50446f7dfe1393c9328025411dda2c623cc8c2
clarkt <clarktanglei@163.com>
2017-09-01 09:43:05 +0800

    将 dir 整合进 build 中

--- compiler/lib/module/builder/index.js
+++ compiler/lib/module/builder/index.js
@@ -13,39 +13,52 @@ import {
     AFTER_STORE,
     BEFORE_BUILD,
     AFTER_BUILD
-} from '../plugin';
+} from '../hook/stage';
 
-export default function (app, addModule) {
+export default function (app) {
     const builder = {
-        async build(list) {
+        async build(sources) {
+
+        },
+
+        async getBuildRequiredInfos(sources) {
+
+        },
+
+        async buildDoc(list) {
             let {parser, store, plugin} = app.module;
 
             list = list.filter(info => path.extname(info.dir) === '.md');
 
             let toBuild = await plugin.exec(BEFORE_BUILD, list.slice(0), list);
 
-            let {toUpdate = [], toDelete = []} = classify(toBuild, ({type}) => {
-                return type === 'delete' ? 'toDelete' : 'toUpdate';
+            let {toSet = [], toDel = []} = classify(toBuild, ({type}) => {
+                return type === 'delete' ? 'toDel' : 'toSet';
             });
 
             await Promise.all([
-                ...toUpdate.map(async info => {
-                    let md = await fs.readFile(info.fullDir, 'utf-8');
-                    let html = await parser.parse(md, info);
-
-                    let obj = {html, dir: info.dir};
-
-                    obj = await plugin.exec(BEFORE_STORE, obj, info);
-
-                    await store.set('article', info.dir, obj);
-                    await plugin.exec(AFTER_STORE, obj, info);
-                }),
-                ...toDelete.map(async info => {
-                    await store.delete('article', info.dir);
-                })
+                ...toSet.map(builder.setDoc),
+                ...toDel.map(builder.deleteDoc)
             ]);
 
             await plugin.exec(AFTER_BUILD, toBuild);
+        },
+
+        async setDoc(info) {
+            let plugin = app.module.plugin;
+
+            let md = await fs.readFile(info.fullDir, 'utf-8');
+            let html = await parser.parse(md, info);
+
+            let obj = {html, dir: info.dir};
+
+            obj = await plugin.exec(BEFORE_STORE, obj, info);
+            await store.set('article', info.dir, obj);
+            await plugin.exec(AFTER_STORE, obj, info);
+        },
+
+        async deleteDoc(info) {
+            await app.module.store.delete('article', info.dir);
         }
     };
 
--- compiler/lib/module/hook/event.js
+++ compiler/lib/module/hook/event.js
@@ -15,5 +15,5 @@ export default function (app) {
                 return event;
             }
         }
-    }
+    };
 }
