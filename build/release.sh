60fd578b46ab071306ae989197c8ecba9434836b
clark-t <clarktanglei@163.com>
2017-11-17 11:24:51 +0800

    update version

---
+++ build/release.sh
@@ -0,0 +1,2 @@
+npm run build
+npm publish --registry=http://registry.npmjs.org
--- test/index.js
+++ test/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 var path = require('path');
-var Kram = require('../dist/index').Kram;
+var Kram = require('../lib/index').Kram;
 var fs = require('fs-extra');
 
 
