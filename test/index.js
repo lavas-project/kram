9c2c8dd2b26663446af5f4c1f8a0dbab2eb7b550
tanglei02 <tanglei02@baidu.com>
2018-02-26 14:32:12 +0800

    modify test

--- test/index.js
+++ test/index.js
@@ -5,12 +5,6 @@
 
 /* eslint-disable */
 
-// var os = require('os');
-
-// if (os.type() === 'Windows_NT') {
-//     process.env.NODE_ENV = 'development';
-// }
-
 require('babel-register');
 require('babel-polyfill');
 var cheerio = require('cheerio');
@@ -18,6 +12,10 @@ var cheerio = require('cheerio');
 var path = require('path');
 var Kram = require('../lib/index').Kram;
 var fs = require('fs-extra');
+
+// 测试 plugin
+// 作用是从文章第一段中提取文字作为文章的默认介绍
+// 挂载到 info.description 中
 
 class DefaultDescription {
     apply(on, app) {
@@ -33,54 +31,23 @@ class DefaultDescription {
     }
 }
 
-
 var app = new Kram({
     basePath: path.resolve(__dirname, './tmp'),
     sources: [
         {
-            name: 'lavas',
+            name: 'test',
             loader: 'local',
-            from: path.resolve(__dirname, 'lavas'),
-            // loader: 'downloadGitRepo',
-            // from: 'github:lavas-project/lavas-tutorial',
-            to: path.resolve(__dirname, './tmp/lavas')
-            // ,
-            // tmp: path.resolve(__dirname, './tmp/git/lavas')
-        },
-        {
-            name: 'pwa',
-            loader: 'local',
-            from: path.resolve(__dirname, 'pwa'),
-            // loader: 'downloadGitRepo',
-            // from: 'github:lavas-project/lavas-tutorial',
-            to: path.resolve(__dirname, './tmp/pwa')
-            // ,
-            // tmp: path.resolve(__dirname, './tmp/git/lavas')
+            from: path.resolve(__dirname, 'md'),
+            to: path.resolve(__dirname, './tmp/test')
         }
     ],
     plugin: {
         description: new DefaultDescription()
     },
-    // sources: [
-    //     {
-    //         name: 'test',
-    //         loader: 'local',
-    //         from: path.resolve(__dirname, 'md'),
-    //         // to: './lavas',
-    //         // tmp: './git/lavas'
-    //         to: path.resolve(__dirname, './tmp/test')
-    //     },
-    //     {
-    //         name: 'lalalan',
-    //         loader: 'local',
-    //         from: path.resolve(__dirname, 'lalala'),
-    //         // to: './lavas',
-    //         // tmp: './git/lavas'
-    //         to: path.resolve(__dirname, './tmp/lalalan')
-    //     }
-    // ],
     routes: [
+        // 资源文件（非 markdown）的 url 路由规则
         {
+            // 函数方式判断
             path(filePath) {
                 return path.extname(filePath) !== '.md';
             },
@@ -88,169 +55,47 @@ var app = new Kram({
                 return `/assets/${filePath}`;
             }
         },
+        // 文档文件的 url 路由规则
         {
-            path: /^lavas\/vue\/foundation/,
+            path: /\.md$/,
             url(filePath) {
-                return `/happy/${filePath}`;
-            }
-            // path: '正则 or function or 字符串'
-        },
-        {
-            path: /^lavas\/vue/,
-            url(filePath) {
-                return `/guide/${filePath}`;
-            }
-        },
-        {
-            path: /^lavas\//,
-            url(filePath) {
-                return `/not-any/${filePath}`;
-            }
-        },
-        {
-            path: /^pwa\//,
-            url(filePath) {
-                return `/doc/${filePath}`;
+                let key = filePath.replace(/\.md$/, '');
+                return `/doc/${key}`;
             }
         }
     ]
 });
 
-// app.on('afterFilterFile', function (args) {
-//     console.log('on after filter file');
-//     console.log(args);
-//     console.log('---');
-// });
-app.on('done', function () {
-    console.log('done')
-    // console.log(app.default.config.store.storage.map)
-    // console.log('----')
-    // console.log(app.default.config.store.storage.map['KRAM$$menu$$all'][0])
-    // console.log(app.default.config.store.storage.map['KRAM$$menu$$all'][0].children[2])
-    // console.log('----')
-    // console.log(app.entryInfos)
-    // app.getMenu('pwa').then((info) => {
-    //     console.log(info)
-    // })
-
-    // app.getMenuItem('pwa').then((info) => {
-    //     console.log(info)
-    // })
-    app.store.get('entryInfos', 'entryPaths/lavas').then(info => {
-        console.log('entryPaths/lavas')
-        console.log(info)
-    })
-
-    app.store.get('entryInfos', 'entryPaths/pwa').then(info => {
-        console.log('entryPaths/pwa')
-        console.log(info)
-    })
-
-    app.store.get('entryInfos', 'entryPaths').then(info => {
-        console.log('entryPaths')
-        console.log(info)
-    })
-
-    // app.getDoc('pwa/automatic-login/introduction.md').then((info) => {
-    //     console.log(info)
-    // })
-})
-
-// var md = fs.readFileSync(path.resolve(__dirname, './md/test.md'), 'utf-8');
-// app.parse(md, {
-//     fullDir: path.resolve(__dirname, './md/test.md'),
-//     dir: './md/test.md'
-// }).then(function (html) {
-//     console.log(html);
-// });
-// var tmpPath;
-var removeFilePath = path.resolve(__dirname, './pwa/architecture/remove.md');
-var addFilePath = path.resolve(__dirname, './pwa/architecture/add.md');
-var modifyFilePath = path.resolve(__dirname, './pwa/architecture/modify.md');
-
-Promise.resolve()
-.then(function () {
-    fs.writeFileSync(removeFilePath,
-`# 测试删除
-
-## 测试删除二级标题
-
-测试删除内容
-
-### 测试删除三级标题
-
-测试结尾
-`
-    );
-})
-.then(function () {
-    return app.exec();
-})
-// .then(function () {
-//     var authorPath = path.resolve(__dirname, './md/author.partial.html');
-//     var str = fs.readFileSync(authorPath, 'utf-8');
-//     str += `\n${Date.now()}\n`;
-//     fs.writeFileSync(authorPath, str);
-// })
-.then(function () {
-    return sleep(5000);
-})
-.then(function () {
-    fs.unlinkSync(removeFilePath);
-    fs.writeFileSync(addFilePath,
-`# 测试添加
-
-## 测试添加二级标题
-
-测试结尾
-
-`
-    );
-})
-.then(function () {
-    metaPath = path.resolve(__dirname, './pwa/meta.json');
-    var str = fs.readFileSync(metaPath, 'utf-8');
-    var meta = JSON.parse(str);
-    meta.menu[0].name = '测试文档修改' + Date.now();
-    str = JSON.stringify(meta);
-    fs.writeFileSync(metaPath, str);
-})
-// .then(function () {
-//     return app.exec('test');
-// })
-.then(function () {
-    return sleep(5000);
-})
-// .then(function () {
-//     fs.unlinkSync(tmpPath);
-// })
-.then(function () {
-    return app.exec('pwa');
-})
-.then(function () {
-    return sleep(5000);
-})
-.then(function () {
-    fs.unlinkSync(addFilePath);
-})
-// .then(function () {
-//     console.log(app.module.store.default.storage);
-// })
-// .then(() => {
-//     return app.store.get('article', 'test/test.md');
-// })
-// .then(obj => {
-//     // console.log(app.config.plugin.hooks)
-//     console.log('--------')
-//     console.log(obj)
-// })
-.catch(function (err) {
-    console.log('in error');
-    console.log(err);
+app.on('start', function () {
+    console.log('--- on start ---');
 });
 
-function sleep(time) {
-    return new Promise(function (resolve) {
-        setTimeout(resolve, time);
+app.on('done', function () {
+    console.log('--- on done ---');
+});
+
+app.exec().then(function () {
+    app.getDoc('test/normal.md').then(info => {
+        console.log('** test/normal.md **');
+        console.log(JSON.stringify(info, null, 4));
+        console.log('');
     });
-}
+
+    app.getDoc('test/enhance.md').then(info => {
+        console.log('** test/enhance.md **');
+        console.log(JSON.stringify(info, null, 4));
+        console.log('');
+    });
+
+    app.getMenu('test').then(info => {
+        console.log('** get menu: test **');
+        console.log(JSON.stringify(info, null, 4));
+        console.log('');
+    });
+
+    app.getMenu('test/sub').then(info => {
+        console.log('** get menu: test/sub **');
+        console.log(JSON.stringify(info, null, 4));
+        console.log('');
+    });
+});
